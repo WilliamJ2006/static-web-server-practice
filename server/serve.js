e5d0966cda4a9f508042239e8912bd1d32bf4b1b
William <william.jiang1101@gmail.com>
2026-03-05 15:10:49 -0500

    Fixed

--- server/serve.js
+++ server/serve.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 8080;
+const port = 8080;
 
 const pathToFrontend = path.join(__dirname, '../frontend/dist');
 const serveStatic = express.static(pathToFrontend);
 app.use(serveStatic);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
